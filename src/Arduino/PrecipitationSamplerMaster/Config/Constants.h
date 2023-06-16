672bd87cd30969c5a2955f7df47ed3c5fece1407
Mathias Madsen <135036238+TheEngibeer@users.noreply.github.com>
2023-06-16 13:02:16 +0200

    commit

---
+++ src/Arduino/PrecipitationSamplerMaster/Config/Constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../modules/setup/modules.h"
+
+// ------------------------------------------------------------ //
+//                          Sampler                             //
+// ------------------------------------------------------------ //
+
+// Et eller andet
+boolean switchstate[3];          // Holder for switches
+uint8_t height_down_steps = 400; // how many steps the height stepper wil lower the gantry
---
+++ src/Arduino/PrecipitationSamplerMaster/modules/system/control.cpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../Components/DS3231.cpp"
+#include "../setup/modules.h"
+
+// Enables Primary Systems
+void SystemEnablePrimary() {
+}
+
+void SlaceSystemEnablePrimary() {
+}
+
+// Enables Secondary Systems
+void SystemEnableSecondary() {
+}
+
+// Disable Secondary Systems
+void SystemDisableSecondary() {
+}
